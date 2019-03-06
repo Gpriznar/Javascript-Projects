bee4d36cc4ccae511aa5b43481a649f67ec4fa38
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-03-05 23:21:49 -0600

    Add files via upload
    Fixed remove button and added additional CSS styling

--- ToDo-List/todo.js
+++ ToDo-List/todo.js
@@ -4,17 +4,26 @@ let assignmentboxtext = document.getElementById("assignmentboxtext")
 let completedtasks = document.getElementById("completedtasks")
 
 
+
 addtask.addEventListener('click',function() {
+
   var tasklist = document.createElement("li")
   pendingtasks.appendChild(tasklist)
-  var x = document.createElement("INPUT")
-   x.setAttribute("type", "checkbox")
+
+  var x = document.createElement("input")
+  x.setAttribute("type", "checkbox")
   var para = document.createElement("p")
   var tasktext = document.createTextNode(assignmentboxtext.value)
-   para.appendChild(tasktext)
+  para.appendChild(tasktext)
+
   var btn = document.createElement("button")
   var btntext = document.createTextNode("Remove")
-   btn.appendChild(btntext)
+  btntext.id = "removebuttontext"
+  btn.onclick = function removetask() {
+    event.currentTarget.parentElement.remove();
+  }
+
+  btn.appendChild(btntext)
   tasklist.appendChild(x)
   tasklist.appendChild(para)
   tasklist.appendChild(btn)
